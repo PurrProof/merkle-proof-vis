7df99b759edbfde88f12542658008eb086869c18
PurrProof <149718167+PurrProof@users.noreply.github.com>
2024-09-30 14:41:54 +0200

    fix link to calldata visualizer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
         <span className="license"><a href="https://github.com/PurrProof/merkle-proofs-vis?tab=MIT-1-ov-file#readme">MIT License</a></span>
         <span className="copy">Copyright (c) 2024 <a href="https://github.com/PurrProof">PurrProof</a></span>
         <span className="credits"><a href="https://github.com/PurrProof/merkle-proofs-vis?tab=readme-ov-file#credits">Credits</a></span>
-        <span>See also: <a href="https://github.com/PurrProof/calldata-visualizer">ABI Calldata Layout Visualizer</a></span>
+        <span>See also: <a href="https://purrproof.github.io/calldata-visualizer/">ABI Calldata Layout Visualizer</a></span>
     </footer>)
 
 export default Footer;
